cd7d02ed64936d0f552c16563a6f8c2da16a557a
yassinebid <85518784+yassinebid@users.noreply.github.com>
2022-04-06 16:12:09 +0100

    fix

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,14 @@
 #!/usr/bin/node
-const x = process.argv[2];
+const x = Number(process.argv[2]);
 if (isNaN(x)) {
-  console.log("Missing size");
+  console.log('Missing size');
 } else {
-  for (let i = 0; i < x; i++) {
-    console.log("X".repeat(x));
+  let i;
+  let s = '';
+  for (i = 0; i < x; i++) {
+    s += 'X';
+  }
+  for (i = 0; i < x; i++) {
+    console.log(s);
   }
 }
