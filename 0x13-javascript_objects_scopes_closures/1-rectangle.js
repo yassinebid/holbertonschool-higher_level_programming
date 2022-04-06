26d5af63f338051858d728dedd90a342aa151d6d
yassinebid <85518784+yassinebid@users.noreply.github.com>
2022-04-06 16:19:19 +0100

    fix

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
+class Rectangle {
   constructor (width, height) {
 	  this.width = width;
 	  this.height = height;
 	}
-};
+}
+module.exports = Rectangle;;
